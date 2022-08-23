ab4a09bedb2c5c0ef5d87a3d5082127d421c99fb
Camilo Jimenez <camilojj14@gmail.com>
2022-08-23 13:42:46 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 const upload = require('./multer')
 require('dotenv').config()
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html')
 });
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
@@ -25,4 +25,4 @@ app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Your app is listening on port ' + port)
-});
+})
